70bddf64971c43d6d14adc704a3825ff7ce8b019
ferney1234567 <160661202+ferney1234567@users.noreply.github.com>
2024-05-23 10:51:01 -0500

    Add files via upload
    enviar ejemplos guia04

---
+++ ciclosGuia04/ejemplos/ejemploWhile01.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  //variable de tipo contador
+
+  // WHILE EJEMPLO DONDE SE CONOCE LA CANTIDAD DE ITERACIONES
+  int contador = 0;
+  while (contador <= 5) {
+    print("valor: $contador");
+    contador ++;//se incremanta 1
+    
+  }
+}
