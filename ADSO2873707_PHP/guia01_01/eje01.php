fe35dc857a33491ce4a318d24fd172805755276a
ferney1234567 <160661202+ferney1234567@users.noreply.github.com>
2024-09-30 16:29:02 -0500

    Add files via upload

---
+++ ADSO2873707_PHP/guia01_01/eje01.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title> Ejemplo usando php</title>
+        <style type="text/css">
+            p{ color:white;}
+            #contenedor{background-color: green; width: 300px; text-align: center; margin: auto; padding: 15px;}
+        </style>
+    </head>
+    <body>
+        <?php
+        echo "<div id='contenedor'>";
+        echo "<p> INFORMACION</p>";
+        echo "</div>";
+        ?>
+    </body>
+</html>
